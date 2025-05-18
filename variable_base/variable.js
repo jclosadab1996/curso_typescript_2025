25c38f1c4f62eda4abc762281626ca9e38f14630
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-05-18 00:31:42 -0500

    feat: Initialize React project with Vite and TypeScript setup
    - Added package.json with dependencies for React, ReactDOM, and development tools.
    - Included Vite configuration for building and serving the application.
    - Created initial React components and styles for the application.
    - Set up TypeScript configuration for the project.
    - Implemented basic HTML structure for the web application.
    - Developed JavaScript and TypeScript files demonstrating variable usage and class implementation.

---
+++ variable_base/variable.js
@@ -0,0 +1,6 @@
+var nombre = "Juan";
+var edad = 30;
+var esDesarrollador = true;
+var mes;
+var variableNull = null;
+console.log("Hola + ".concat(nombre));
---
+++ variable_base/variable.ts
@@ -0,0 +1,7 @@
+let nombre: string = "Juan";
+let edad: number = 30;
+let esDesarrollador: boolean = true;
+let mes: string | undefined;
+let variableNull: null = null;
+
+console.log(`Hola + ${nombre}`);
