23a7c07397884ee8d282681c9af5e6c3b216c255
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-05-27 19:50:49 -0500

    style: Format code for consistency and improve comments clarity

--- Variables_diferentes/main.ts
+++ Variables_diferentes/main.ts
@@ -1,27 +1,28 @@
-let variableAny : any;
+let variableAny: any; // La sentencia any permite asignar cualquier tipo de valor a la variable, lo que puede ser útil en algunos casos, pero también puede llevar a errores si no se maneja con cuidado.
 //variableAny = "Hello";
 variableAny = 42;
 //variableAny = true;
 console.log("Valor de variable any " + variableAny);
 
-let variableDesconocida: unknown;
+let variableDesconocida: unknown; // La sentencia unknown es más segura que any porque no permite que se asignen valores de tipos no definidos.
 
 //variableDesconocida = "Hello";
 variableDesconocida = 42;
 //variableDesconocida = true;
 
 if (typeof variableDesconocida === "string") {
-    console.log("Variable unknown is a string:", variableDesconocida);
+  console.log("Variable unknown is a string:", variableDesconocida);
 }
 
-/*function throwError(message: string): never {
+/*function throwError(message: string): never { // La sentencia never indica que la función nunca regresará, lo que puede ser útil para funciones que lanzan excepciones o entran en un bucle infinito.
     throw new Error(message);
 }
 
 throwError("Este es un error");*/
 
 function logMessage(message: string): void {
-    console.log("Log message:", message);
+  // La sentencia void indica que la función no regresará ningún valor, lo que es útil para funciones que realizan una acción pero no necesitan devolver un resultado.
+  console.log("Log message:", message);
 }
 
 logMessage("Este es un mensaje de log");
